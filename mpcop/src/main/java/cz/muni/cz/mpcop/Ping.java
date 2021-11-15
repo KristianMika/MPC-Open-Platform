3a7588dd1b06fedf5208d56c7392f70ab5348024
Kristian Mika <xmika2@fi.muni.cz>
2021-11-15 22:52:14 +0100

    feat: ping performance ping applets asynchronously

--- mpcop/src/main/java/cz/muni/cz/mpcop/Ping.java
+++ mpcop/src/main/java/cz/muni/cz/mpcop/Ping.java
@@ -8,26 +8,41 @@ import javax.smartcardio.CommandAPDU;
 import javax.smartcardio.ResponseAPDU;
 import java.util.Arrays;
 import java.util.Objects;
+import java.util.Optional;
 
 import static cz.muni.cz.mpcop.JavacardUtils.checkSw;
 
-public class Ping {
+public class Ping implements Runnable {
+    public static final byte NONE = 0x00;
     private final static byte INS_PING = (byte) 0x1234;
     private static final byte CLA_PING = (byte) 0x88;
-    public static final byte NONE = 0x00;
+    public volatile Optional<String> lastError = Optional.empty();
 
-    private CardChannel channel ;
+    private final CardChannel channel;
+
     public Ping(CardChannel channel) {
         this.channel = channel;
     }
 
     public void ping() throws CardException, GeneralMPCOPException {
+
         ResponseAPDU response = channel.transmit(new CommandAPDU(CLA_PING, INS_PING, NONE, NONE));
         checkSw(response);
         byte[] data = response.getData();
         String dataString = new String(Arrays.copyOfRange(data, 0, 4));
         if (!Objects.equals(dataString, "pong")) {
             throw new GeneralMPCOPException("Did not return pong");
+        }
+
+    }
+
+    @Override
+    public void run() {
+        try {
+            this.ping();
+            // Thread.sleep(1000);
+        } catch (Exception e) {
+            this.lastError = Optional.ofNullable(e.getMessage());
         }
     }
 }
--- mpcop/src/main/java/cz/muni/cz/mpcop/PingManager.java
+++ mpcop/src/main/java/cz/muni/cz/mpcop/PingManager.java
@@ -8,6 +8,7 @@ import javax.smartcardio.CardChannel;
 import javax.smartcardio.CardException;
 import javax.smartcardio.TerminalFactory;
 import java.util.ArrayList;
+import java.util.Optional;
 
 public class PingManager {
     private final static String appletID = "01ffff0405060708090102";
@@ -26,18 +27,41 @@ public class PingManager {
     }
 
     public ArrayList<Long> ping() throws CardException, GeneralMPCOPException {
-        // TODO: run in parallel
+
         ArrayList<Long> durations = new ArrayList<>();
+        ArrayList<Thread> threads = new ArrayList<>();
         while (durations.size() < pingPlayers.size()) {
             durations.add(0L);
         }
 
         for (int i = 0; i < pingPlayers.size(); i++) {
             long operationStart = System.currentTimeMillis();
+
+
             for (int j = 0; j <= i; j++) {
-                pingPlayers.get(j).ping();
+                Ping player = pingPlayers.get(j);
+                Thread t = new Thread(player);
+                t.start();
+                threads.add(t);
             }
+
+            threads.forEach((Thread t) -> {
+                try {
+                    t.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+
             durations.set(i, System.currentTimeMillis() - operationStart);
+
+            for (Ping player : pingPlayers) {
+                Optional<String> lastError = player.lastError;
+                if (lastError.isPresent()) {
+                    throw new GeneralMPCOPException(lastError.get());
+                }
+            }
+
         }
         return durations;
     }
